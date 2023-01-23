4209b2734f3e7f204602c2a2d9f00669bf096147
arrrky <arkanesobaka@gmail.com>
2023-01-23 21:52:59 +0500

    Add base .NET MAUI template

---
+++ 2lz/2lz/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace _2lz;
+
+public partial class MainPage : ContentPage
+{
+	private const string PROJECT_GIT_URL = "https://github.com/arrrky/2lz";
+
+    public MainPage()
+	{
+		InitializeComponent();
+	}	
+
+    private void OnGitLinkButton_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+            Browser.Default.OpenAsync(PROJECT_GIT_URL);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+        }
+    }
+}
---
+++ 2lz/2lz/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace _2lz;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
