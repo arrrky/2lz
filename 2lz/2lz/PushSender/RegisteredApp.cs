1ee16b5c1b4dc7b79ec6e0e181240a979f314c53
arrrky <arkanesobaka@gmail.com>
2023-02-21 00:05:45 +0500

    Add RegisteredApp class

---
+++ 2lz/2lz/PushSender/RegisteredApp.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace _2lz.PushSender
+{
+    [Table("app")]
+    public class RegisteredApp
+    {
+        [PrimaryKey, Unique, Column("_name")]
+        public string Name { get; set; }
+
+        [Unique, Column("_id")]
+        public string Id { get; set; }
+    }
+}
